4873781c097128d720b4b3e479ad516548ce61c8
Gabriel Trindade <icandoitgrt@gmail.com>
2023-02-18 00:27:04 -0300

    conceitos básicos de poo em csharp

---
+++ byteBank/byteBank/ContaCorrente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+
+        public void dadosConta()
+        {
+            Console.WriteLine("Títular: " + this.titular);
+            Console.WriteLine("Conta: " + this.conta);
+            Console.WriteLine("Dígito: " + this.numero_agencia);
+            Console.WriteLine("Saldo: R$" + this.saldo);
+            Console.WriteLine("\r\n");
+        }
+
+        public void deposito(double valor)
+        {
+            this.saldo = saldo + valor;
+            Console.WriteLine("Saldo atual: R$" + this.saldo);
+        }
+
+        public void saque(double valor)
+        {
+            if(valor > this.saldo)
+            {
+                Console.WriteLine("Valor de saque maior do que valor disponível para saque.");
+                Console.WriteLine("Saldo atual: R$" + this.saldo);
+            }
+            else
+            {
+                this.saldo = saldo - valor;
+                Console.WriteLine("Saldo atual: R$" + this.saldo);
+            }
+
+        }
+
+        public void traferir(ContaCorrente destino, double valor)
+        {
+            if (valor > this.saldo)
+            {
+                Console.WriteLine("Valor de transferência maior do que valor disponível.");
+                Console.WriteLine("Saldo atual: R$" + this.saldo);
+            }
+            else
+            {
+                this.saldo = saldo - valor;
+                destino.deposito(valor);
+                Console.WriteLine("Saldo atual: R$" + this.saldo);
+            }
+        }
+    }
+}
---
+++ byteBank/byteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using byteBank;
+
+ContaCorrente conta1 = new ContaCorrente();
+ContaCorrente conta2 = new ContaCorrente();
+
+
+conta1.titular = "Gabriel";
+conta1.conta = "1315";
+conta1.saldo = 100;
+conta1.numero_agencia = 09;
+
+conta1.dadosConta();
+
+Console.WriteLine("Saque de RS75");
+conta1.saque(75.00);
+
+Console.WriteLine("\r\n");
+
+Console.WriteLine("Depósito de RS200");
+conta1.deposito(200.00);
+
+Console.WriteLine("\r\n");
+
+conta2.titular = "Marcos";
+conta2.conta = "2136";
+conta2.saldo = 50;
+conta2.numero_agencia = 02;
+
+
+conta2.dadosConta();
+
+
+Console.WriteLine("TRASFERÊNCIA DA CONTA 1 PARA CONTA 2\r\n");
+conta1.traferir(conta2, 80);
+
+Console.WriteLine("DADOS DA CONTA 2\r\n");
+conta2.dadosConta();
+
